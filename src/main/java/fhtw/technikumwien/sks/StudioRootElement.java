acfbbe68b9d515706224cb3a1cfa0f69d83fe8c1
Florian Reiter <flow.reiter@gmail.com>
2016-11-28 14:22:29 +0100

    Übung 6

---
+++ src/main/java/fhtw/technikumwien/sks/StudioRootElement.java
@@ -0,0 +1,27 @@
+package fhtw.technikumwien.sks;
+
+import fhtw.sks.generated.Studio;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by Flo on 28/11/2016.
+ */
+@XmlRootElement(name = "studios")
+public class StudioRootElement {
+
+    private List<Studio> studios;
+
+    public StudioRootElement() {
+    }
+
+    public List<Studio> getStudios() {
+        return studios;
+    }
+
+    public void setStudios(List<Studio> studios) {
+        this.studios = studios;
+    }
+}
